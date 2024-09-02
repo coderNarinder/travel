01b7ce2273bf2e005ba981c7accb5676841a45dc
Sukhraj Singh <sukhrajsingh101@gmail.com>
2024-09-02 21:01:27 +0530

    UI fixes

--- src/pages/home/homepage/index.jsx
+++ src/pages/home/homepage/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
+
+import './../../../../public/css/bootstrap.min.css';
+import './../../../../public/css/jquery-ui.min.css';
+import './../../../../public/css/ace-responsive-menu.css';
+import './../../../../public/css/menu.css';
+import './../../../../public/css/fontawesome.css';
+import './../../../../public/css/flaticon.css';
+import './../../../../public/css/bootstrap-select.min.css';
+import './../../../../public/css/animate.css';
+import './../../../../public/css/slider.css';
+import './../../../../public/css/style.css';
+import './../../../../public/css/ud-custom-spacing.css';
+import './../../../../public/css/responsive.css';
+
 const HomePage = () => {
     return (
         <>
